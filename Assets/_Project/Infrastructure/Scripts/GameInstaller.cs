35cdf783c815cd18fc08ade390e0a046caf2f2d0
Mishanych <myhaylo03@gmail.com>
2026-04-04 00:19:17 +0300

    added camera following

--- Assets/_Project/Infrastructure/Scripts/GameInstaller.cs
+++ Assets/_Project/Infrastructure/Scripts/GameInstaller.cs
@@ -1,4 +1,6 @@
+using ArmyCommander.Camera;
 using ArmyCommander.Input;
+using ArmyCommander.Player;
 using UnityEngine;
 using Zenject;
 
@@ -7,10 +9,15 @@ namespace ArmyCommander.Infrastructure
     public class GameInstaller : MonoInstaller
     {
         [SerializeField] private JoystickInputService _joystick;
+        [SerializeField] private PlayerMovement _player;
+        [SerializeField] private CameraFollower _cameraFollower;
 
         public override void InstallBindings()
         {
+            Container.Bind<PlayerMovement>().FromInstance(_player).AsSingle();
+            Container.Bind<CameraFollower>().FromInstance(_cameraFollower).AsSingle();
             Container.Bind<IInputService>().FromInstance(_joystick).AsSingle();
+            
             Container.BindInterfacesTo<GameStartup>().AsSingle();
         }
     }
---
+++ Assets/_Project/Modules/Camera/Scripts/CameraFollower.cs
@@ -0,0 +1,41 @@
+﻿using ArmyCommander.Player;
+using UnityEngine;
+using Zenject;
+
+namespace ArmyCommander.Camera
+{
+    public class CameraFollower : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private Vector3 _offset = new Vector3(0, 15, -10);
+        [SerializeField] private float _smoothTime = 0.25f;
+
+        private Transform _currentTarget;
+        private Vector3 _currentVelocity = Vector3.zero;
+
+        public void SetTarget(Transform target)
+        {
+            _currentTarget = target;
+        }
+
+        [Inject]
+        public void Construct(PlayerMovement player)
+        {
+            SetTarget(player.transform);
+        }
+
+        private void LateUpdate()
+        {
+            if (_currentTarget == null) return;
+
+            Vector3 targetPosition = _currentTarget.position + _offset;
+
+            transform.position = Vector3.SmoothDamp(
+                transform.position, 
+                targetPosition, 
+                ref _currentVelocity, 
+                _smoothTime
+            );
+        }
+    }
+}
